8f814e331b1283878e9f1762f790a560a4dd42ff
HaekyungSeong <haekyungs95@gmail.com>
2023-04-07 13:04:12 +0900

    new Date 를 이용한 시계 제작

--- index.js
+++ index.js
@@ -19,3 +19,34 @@ root.children[0].style.textAlign = "center";
 root.children[0].style.fontFamily = "Poor Story";
 root.children[0].style.background = "white"
 
+const timeData = () => {
+  const date = new Date();
+  const day = [
+    "월요일",
+    "화요일",
+    "수요일",
+    "목요일",
+    "금요일",
+    "토요일",
+    "일요일",
+  ];
+
+  const nowdate =
+    date.getFullYear() +
+    "년 " +
+    (date.getMonth() + 1) +
+    "월 " +
+    date.getDate() +
+    "일 " +
+    day[date.getDay()-1];
+  const nowtime =
+    date.getHours() +
+    "시 " +
+    date.getMinutes() +
+    "분 " +
+    date.getSeconds() +
+    "초 ";
+  root.children[0].innerHTML = nowdate + "<br/>" + nowtime;
+};
+
+setInterval(timeData, 1000);
